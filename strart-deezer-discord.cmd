dd3959eb162a88b0ada26ed81ac17e4afcaad559
evan <122116894+Evanpersonyt@users.noreply.github.com>
2025-08-07 16:07:58 +0200

    Update strart-deezer-discord.cmd

--- strart-deezer-discord.cmd
+++ strart-deezer-discord.cmd
@@ -2,5 +2,4 @@
 set "FOLDER=C:\Users\evanb\Desktop\Mes-Extensions\detecte-Music-go-discord\app"
 set "CMD=npm start"
 
-:: Ouvre une invite de commandes en administrateur et exécute les commandes
 powershell -Command "Start-Process cmd -ArgumentList '/k cd /d \"%FOLDER%\" && %CMD%' -Verb RunAs"
